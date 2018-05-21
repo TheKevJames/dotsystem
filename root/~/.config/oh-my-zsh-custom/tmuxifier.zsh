d8674f47f15549cd9a5276c8824034781e7da827
Kevin James <KevinJames@thekev.in>
2018-05-21 14:20:16 -0700

    fix(tmuxifier): fix typo

--- root/~/.config/oh-my-zsh-custom/tmuxifier.zsh
+++ root/~/.config/oh-my-zsh-custom/tmuxifier.zsh
@@ -1,5 +1,5 @@
 export TMUXIFIER="/opt/tmuxifier"
 export TMUXIFIER_LAYOUT_PATH="${XDG_CONFIG_HOME}/tmuxifier/layouts"
-export PATH="${$TMUXIFIER}/bin:${PATH}"
+export PATH="${TMUXIFIER}/bin:${PATH}"
 
 alias mux="tmuxifier"
