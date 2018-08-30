f098568d24140fc51111b03233fb73a02f8eff10
Kevin James <KevinJames@thekev.in>
2018-08-30 16:40:00 -0700

    feat(cli): update some builtins

---
+++ root/~/.config/oh-my-zsh-custom/builtins.zsh
@@ -0,0 +1,14 @@
+# https://the.exa.website/
+alias l='exa -la'
+alias la='exa -la'
+alias ll='exa -l'
+alias ls='exa'
+alias lsa='exa -la'
+
+# https://github.com/sharkdp/bat
+alias cat='bat'
+
+alias top='htop'
+
+# http://denilson.sa.nom.br/prettyping/
+alias ping='prettyping'
