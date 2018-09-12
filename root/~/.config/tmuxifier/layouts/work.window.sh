fdcc9db79983afc425d524afceb142223539dee4
Kevin James <kevin@talkiq.com>
2018-09-11 18:25:43 -0700

    feat(tmuxifier): randomize default playlist

--- root/~/.config/tmuxifier/layouts/work.window.sh
+++ root/~/.config/tmuxifier/layouts/work.window.sh
@@ -5,7 +5,7 @@ select_pane 0
 split_h 50
 
 run_cmd "bugwarrior-pull" 0
-run_cmd "youtube-viewer -n \":playlist=LLRvXhm62yMVj8sJOJQYpRGg\"" 1
+run_cmd "youtube-viewer -s -n \":playlist=LLRvXhm62yMVj8sJOJQYpRGg\"" 1
 
 send_keys "task next" 0
 send_keys "1.." 1
