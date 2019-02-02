455923152f159cd34f9413ad23dba4dceb587bdf
Kevin James <kevin@talkiq.com>
2019-02-02 11:50:32 -0800

    chore(mux): update work window

--- root/~/.config/tmuxifier/layouts/work.window.sh
+++ root/~/.config/tmuxifier/layouts/work.window.sh
@@ -4,10 +4,10 @@ split_v 74
 select_pane 0
 split_h 50
 
-run_cmd "bugwarrior-pull" 0
-run_cmd "youtube-viewer -s -n \":playlist=LLRvXhm62yMVj8sJOJQYpRGg\"" 1
+run_cmd "task next +work" 0
+run_cmd "youtube-viewer -s -n -L" 1
 
-send_keys "task next" 0
-send_keys "1.." 1
+send_keys "task next +work" 0
+send_keys ":anp 1.." 1
 
 select_pane 2
