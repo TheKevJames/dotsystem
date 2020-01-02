44cb2b2ba31316bca2d42c9cd648ff0023bc80b2
Kevin James <KevinJames@thekev.in>
2020-01-01 19:01:27 -0600

    feat(gcloud): force py3 usage

--- root/~/.config/zsh/dropins/gcloud.zsh
+++ root/~/.config/zsh/dropins/gcloud.zsh
@@ -1,3 +1,6 @@
 source "${XDG_SRC_HOME}/google-cloud-sdk/completion.zsh.inc"
 
 export PATH="${XDG_SRC_HOME}/google-cloud-sdk/bin:${PATH}"
+
+# otherwise it prioritizes py2 over py3
+export CLOUDSDK_PYTHON=python3
