e86d5c61756a7abe1eb02495e3508e6b6e1ad963
Kevin James <KevinJames@thekev.in>
2020-01-03 15:57:44 -0600

    feat(shell): install fzf

---
+++ root/~/.config/zsh/dropins/fzf.zsh
@@ -0,0 +1,5 @@
+source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_COMMAND='fd -u --type f'
+export FZF_DEFAULT_OPTS='--height 40% --border'
