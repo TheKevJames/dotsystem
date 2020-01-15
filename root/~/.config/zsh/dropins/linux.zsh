28e1300ce63d14f6ed37a4ef8b43a8cb354a0729
Kevin James <KevinJames@thekev.in>
2020-01-14 19:28:18 -0800

    fix(wsl): fixup bad syntax

--- root/~/.config/zsh/dropins/linux.zsh
+++ root/~/.config/zsh/dropins/linux.zsh
@@ -1,8 +1,10 @@
 if [[ "$OSTYPE" == "linux"* ]]; then
     alias open="xdg-open"
 
-    if [[ -f /proc/version && grep -q Microsoft /proc/version ]]; then
-        # TODO: update to WSL >= v17738
-        unsetopt BG_NICE
+    if [[ -f /proc/version ]]; then
+        if grep -q Microsoft /proc/version; then
+            # TODO: update to WSL >= v17738
+            unsetopt BG_NICE
+        fi
     fi
 fi
