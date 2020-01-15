26a9498472e7adf92462bce1d1bb42cef0c73958
Kevin James <KevinJames@thekev.in>
2020-01-14 19:25:14 -0800

    fix(wsl): disable zsh autonice

--- root/~/.config/zsh/dropins/linux.zsh
+++ root/~/.config/zsh/dropins/linux.zsh
@@ -1,3 +1,8 @@
 if [[ "$OSTYPE" == "linux"* ]]; then
     alias open="xdg-open"
+
+    if [[ -f /proc/version && grep -q Microsoft /proc/version ]]; then
+        # TODO: update to WSL >= v17738
+        unsetopt BG_NICE
+    fi
 fi
