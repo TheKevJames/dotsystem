977a732a4b91ed291622f5040944aa5ec9f539e4
Kevin James <KevinJames@thekev.in>
2020-02-02 22:18:06 -0800

    fix(gcloud): fixup runtime env vars

--- root/~/.config/zsh/dropins/gcloud.zsh
+++ root/~/.config/zsh/dropins/gcloud.zsh
@@ -2,5 +2,6 @@ source "${XDG_SRC_HOME}/google-cloud-sdk/completion.zsh.inc"
 
 export PATH="${XDG_SRC_HOME}/google-cloud-sdk/bin:${PATH}"
 
+# TODO: this seems to break dialbox
 # otherwise it prioritizes py2 over py3
-export CLOUDSDK_PYTHON=python3
+# export CLOUDSDK_PYTHON=python3
