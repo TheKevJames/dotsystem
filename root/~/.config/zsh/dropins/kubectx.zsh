2644e1802cea4123193fbc19423c95a4b798e707
Kevin James <KevinJames@thekev.in>
2020-04-15 17:46:26 -0700

    feat: install kubectx

---
+++ root/~/.config/zsh/dropins/kubectx.zsh
@@ -0,0 +1,6 @@
+# TODO: have to zcompinit this rather than source it
+# https://github.com/ahmetb/kubectx#linux
+## source "${XDG_SRC_HOME}/kubectx/completion/kubectx.zsh"
+## source "${XDG_SRC_HOME}/kubectx/completion/kubens.zsh"
+
+export PATH="${XDG_SRC_HOME}/kubectx:${PATH}"
