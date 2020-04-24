ed85c785b60a99bb2855e408ee7e9285ac468005
Kevin James <KevinJames@thekev.in>
2020-04-24 12:42:35 -0700

    feat(mux): add dashboard window

---
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -0,0 +1,15 @@
+new_window "dash"
+
+split_h 60
+select_pane 0
+split_v 22
+select_pane 2
+split_v 65
+
+# ...yeah.
+run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 0
+run_cmd "youtube-viewer -s -n -L" 1
+run_cmd "jira mine" 2
+run_cmd "echo '# TODO: git notifs'" 3
+
+select_pane 0
