099e45da5c9a75bba8e8a169a9b7d09f8fd69b23
Kevin James <KevinJames@thekev.in>
2020-04-24 19:07:51 -0700

    feat(shell): apply gruvbox consistently

--- root/~/.config/nvim/init.vim
+++ root/~/.config/nvim/init.vim
@@ -2,12 +2,14 @@ set clipboard=unnamedplus  " use system clipboard
 set expandtab              " spaces > tabs
 set hlsearch               " show highlights on search
 set laststatus=0           " hide statusline
+set nofoldenable           " no default folds on startup
 set number                 " show line numbers
 set relativenumber         " line numbers should be relative
 set shiftwidth=4           " tabs are 4 spaces
 set showmatch              " show matching bracket on insertion
 set synmaxcol=512          " set maximum line length for syntax highlighting
 set tags=.git/tags         " hide ctags in `.git` folder
+set termguicolors          " enable true color support
 
 " netrw configuration
 let g:netrw_banner=0       " hide useless banner info
@@ -21,6 +23,8 @@ syntax on                  " show syntax highlighting
 
 call plug#begin('~/.local/share/nvim/plugged')
 
+Plug 'morhetz/gruvbox'  " gruvbox
+
 Plug 'junegunn/fzf', { 'do': './install --bin' }  " fzf
 Plug 'airblade/vim-gitgutter'                     " git
 
@@ -33,5 +37,6 @@ Plug 'rust-lang/rust.vim', { 'for': 'rust' }           " Rust syntax
 call plug#end()
 
 " plugin configuration
+colorscheme gruvbox
 let g:taskwiki_data_location="~/.local/share/taskwarrior"
 let g:vimwiki_list = [{'path': '~/Dropbox/vimwiki'}, {'path': '~/Dropbox/work/vimwiki'}]
--- root/~/.config/zsh/dropins/prompt.zsh
+++ root/~/.config/zsh/dropins/prompt.zsh
@@ -1,3 +1,25 @@
+# TODO: can we do this by updating the `fg`/`bg` dicts instead?
+local gruvbox_bg=$'\e[38;2;40;40;40m'
+local gruvbox_fg=$'\e[38;2;235;219;178m'
+
+local gruvbox_black=$'\e[38;2;40;40;40m'
+local gruvbox_red=$'\e[38;2;204;36;29m'
+local gruvbox_green=$'\e[38;2;152;151;26m'
+local gruvbox_yellow=$'\e[38;2;215;153;33m'
+local gruvbox_blue=$'\e[38;2;69;133;136m'
+local gruvbox_magenta=$'\e[38;2;177;98;134m'
+local gruvbox_cyan=$'\e[38;2;104;157;106m'
+local gruvbox_white=$'\e[38;2;168;153;132m'
+
+local gruvbox_black_bright=$'\e[38;2;146;131;116m'
+local gruvbox_red_bright=$'\e[38;2;251;73;52m'
+local gruvbox_green_bright=$'\e[38;2;184;187;38m'
+local gruvbox_yellow_bright=$'\e[38;2;250;189;47m'
+local gruvbox_blue_bright=$'\e[38;2;131;165;152m'
+local gruvbox_magenta_bright=$'\e[38;2;211;134;155m'
+local gruvbox_cyan_bright=$'\e[38;2;142;192;124m'
+local gruvbox_white_bright=$'\e[38;2;235;219;178m'
+
 # allow comments in commands
 setopt interactivecomments
 
@@ -7,10 +29,7 @@ setopt prompt_subst
 # use fish-style paths, see ../plugins/shrink-path.plugin.zsh
 zstyle :prompt:shrink_path fish no
 
-# TODO: could add "@${LINENO}"
-local return_code="%(?..%{$fg[red]%} %?%{$reset_color%})"
+# TODO: could add "@${LINENO}" -- need to figure out late binding
+local return_code="%(?..%{$gruvbox_red%} %?%{$reset_color%})"
 
-# user@domain:
-#     if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="green"; fi
-#     %{$fg[$NCOLOR]%}%n%{$fg[green]%}@%m%{$reset_color%}
-PROMPT='$(shrink_path -l -t)${return_code} %{$fg[red]%}%(!.#.»)%{$reset_color%} '
+PROMPT=$'%{$gruvbox_fg%}$(shrink_path -l -t)${return_code} %{$gruvbox_green%}%(!.#.»)%{$reset_color%} '
