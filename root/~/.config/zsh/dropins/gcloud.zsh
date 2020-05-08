85165b2052401f9f3efa7c826376dad7373a07bb
Kevin James <KevinJames@thekev.in>
2020-05-08 15:09:42 -0700

    fix(osx): fixup gcloud dialbox oddity

--- root/~/.config/zsh/dropins/gcloud.zsh
+++ root/~/.config/zsh/dropins/gcloud.zsh
@@ -2,6 +2,5 @@ source "${XDG_SRC_HOME}/google-cloud-sdk/completion.zsh.inc"
 
 export PATH="${XDG_SRC_HOME}/google-cloud-sdk/bin:${PATH}"
 
-# TODO: this seems to break dialbox
 # otherwise it prioritizes py2 over py3
-# export CLOUDSDK_PYTHON=python3
+export CLOUDSDK_PYTHON=python3
