36591242675e13123a69c5a27937003f0d5d42af
Kevin James <KevinJames@thekev.in>
2020-10-26 14:23:57 -0700

    chore(tmux): plug in cctui dashboard

--- root/~/.config/tmuxifier/layouts/dash.window.sh
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -10,6 +10,6 @@ split_v 65
 run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 0
 run_cmd "youtube-viewer -s -n -L" 1
 run_cmd "jira mine" 2
-run_cmd "echo '# TODO: git notifs'" 3
+run_cmd "cctui" 3
 
 select_pane 0
