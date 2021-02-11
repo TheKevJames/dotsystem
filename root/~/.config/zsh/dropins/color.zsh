1430934b980c58fff211d4980e45540c7f9a9ee6
Kevin James <KevinJames@thekev.in>
2021-02-11 12:38:11 -0800

    feat(ssh): colorssh poc

--- root/~/.config/zsh/dropins/color.zsh
+++ root/~/.config/zsh/dropins/color.zsh
@@ -1,1 +1,11 @@
 autoload -U colors && colors
+
+# TODO: colorize based on hostname
+# TODO: support for eg. `gcloud compute ssh ...`
+# TODO: alias as ssh
+function colorssh() {
+  sd '^colors: \*\w+$' 'colors: *red' ~/.config/alacritty/alacritty.yml
+  trap "sd '^colors: \*\w+$' 'colors: *default' ~/.config/alacritty/alacritty.yml" EXIT
+
+  ssh $*
+}
