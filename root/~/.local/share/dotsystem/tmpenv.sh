fe92b952a32271af9bc04c815ec53b1278a7f24a
Kevin James <KevinJames@thekev.in>
2021-05-18 15:49:26 -0700

    fix(bin): remove broken tmpenv source check

--- root/~/.local/share/dotsystem/tmpenv.sh
+++ root/~/.local/share/dotsystem/tmpenv.sh
@@ -1,8 +1,3 @@
-if [[ "$(basename -- "$0")" == "tmpenv.sh" ]]; then
-    echo "tmpenv.sh must be sourced; run 'tmpenv' directly" >&2
-    exit 1
-fi
-
 DIR=$(mktemp -d -t $(basename "${0}"))
 python3 -m venv "${DIR}"
 source "${DIR}/bin/activate"
