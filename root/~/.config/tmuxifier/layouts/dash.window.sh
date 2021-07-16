028fca7e91959695324a98a1c338c4362ba8f724
Kevin James <KevinJames@thekev.in>
2021-07-16 14:51:15 -0700

    feat(audio): switch music from youtube-viewer to spt
    spt is still manually set up for now, since osx has weird edge cases to
    sort out (as per usual).

--- root/~/.config/tmuxifier/layouts/dash.window.sh
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -2,14 +2,14 @@ new_window "dash"
 
 split_h 60
 select_pane 0
-split_v 22
+split_v 30
 select_pane 2
 split_v 65
 
 # ...yeah.
 run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 0
-run_cmd "youtube-viewer -s -n -L" 1
-run_cmd "jira mine" 2
+run_cmd "spt" 1
+run_cmd "ddtui" 2
 run_cmd "cctui" 3
 
 select_pane 0
