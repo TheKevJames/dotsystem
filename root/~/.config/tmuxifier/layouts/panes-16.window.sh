ecde57cfd888d3cd47926f5054ef81b5c12cc50a
Kevin James <KevinJames@thekev.in>
2021-10-08 12:06:42 -0700

    feat(tmux): addd 16 pane mux window

---
+++ root/~/.config/tmuxifier/layouts/panes-16.window.sh
@@ -0,0 +1,32 @@
+new_window "panes-16"
+
+split_v 50
+split_v 50
+select_pane 0
+split_v 50
+
+select_pane 0
+split_h 50
+split_h 50
+select_pane 0
+split_h 50
+
+select_pane 4
+split_h 50
+split_h 50
+select_pane 4
+split_h 50
+
+select_pane 8
+split_h 50
+split_h 50
+select_pane 8
+split_h 50
+
+select_pane 12
+split_h 50
+split_h 50
+select_pane 12
+split_h 50
+
+select_pane 0
