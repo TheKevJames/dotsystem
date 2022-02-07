e6488303899d949efdc5569e8d11e0e9028d3918
Kevin James <KevinJames@thekev.in>
2022-02-07 15:37:05 -0600

    feat(bin): add pin/pout dropins

--- root/~/.config/zsh/dropins/bin.zsh
+++ root/~/.config/zsh/dropins/bin.zsh
@@ -1,1 +1,12 @@
 alias tmpenv="source ${XDG_DATA_HOME}/dotsystem/tmpenv.sh"
+
+# Sync cwd across shells, stolen from:
+# https://github.com/statico/dotfiles/blob/31899c42000219a8b4dbd2b4857af5cb98b38358/.zshrc#L378-L386
+tmpdir="${TMPDIR:-/tmp}"
+pin() {
+  rm -f "${tmpdir}/pindir"
+  (umask 0177; echo $PWD >"${tmpdir}/pindir")
+}
+pout() {
+  cd $(cat "${tmpdir}/pindir")
+}
