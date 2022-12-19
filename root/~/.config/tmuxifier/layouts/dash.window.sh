74bf8302498264b82fde8bcc495056e14e154793
Kevin James <KevinJames@thekev.in>
2022-12-19 17:08:57 -0600

    feat(mux): modify "dash" default commands

--- root/~/.config/tmuxifier/layouts/dash.window.sh
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -8,7 +8,8 @@ split_v 65
 
 # ...yeah.
 run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 0
-run_cmd "spt" 1
+run_cmd "cd ~/src/personal/mosura; poetry run uvicorn mosura.app:app --reload" 1
+run_cmd "task due" 2
 run_cmd "cctui" 3
 
 select_pane 2
