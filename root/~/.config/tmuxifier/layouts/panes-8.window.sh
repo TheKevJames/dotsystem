f0be5908a9e7a40168cb6dc59b90521a9df95d00
Kevin James <KevinJames@thekev.in>
2022-12-23 13:16:18 -0600

    feat(mux): add 8 pane layout

---
+++ root/~/.config/tmuxifier/layouts/panes-8.window.sh
@@ -0,0 +1,17 @@
+new_window "panes-8"
+
+split_v 50
+split_v 50
+select_pane 0
+split_v 50
+
+select_pane 0
+split_h 50
+select_pane 2
+split_h 50
+select_pane 4
+split_h 50
+select_pane 6
+split_h 50
+
+select_pane 0
