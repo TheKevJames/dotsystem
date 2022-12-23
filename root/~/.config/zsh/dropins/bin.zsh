05ed18faf4633b7fdca88b634e4b74e53155912f
Kevin James <KevinJames@thekev.in>
2022-12-23 13:15:49 -0600

    feat(tmpenv): allow chdir and python version specification

--- root/~/.config/zsh/dropins/bin.zsh
+++ root/~/.config/zsh/dropins/bin.zsh
@@ -1,5 +1,3 @@
-alias tmpenv="source ${XDG_DATA_HOME}/dotsystem/tmpenv.sh"
-
 # Sync cwd across shells, stolen from:
 # https://github.com/statico/dotfiles/blob/31899c42000219a8b4dbd2b4857af5cb98b38358/.zshrc#L378-L386
 tmpdir="${TMPDIR:-/tmp}"
--- root/~/.local/share/dotsystem/tmpenv.sh
+++
@@ -1,3 +0,0 @@
-DIR=$(mktemp -d -t $(basename "${0}"))
-python3 -m venv "${DIR}"
-source "${DIR}/bin/activate"
