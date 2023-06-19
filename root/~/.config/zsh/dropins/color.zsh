258def18bd8e3a9dc0593966da79b07c290da705
Kevin James <KevinJames@thekev.in>
2023-06-19 05:28:39 +0100

    refactor(packages): nuke alacritty special casing
    It's installable via macports, don't need the special brew cask case.

--- root/~/.config/zsh/dropins/color.zsh
+++ root/~/.config/zsh/dropins/color.zsh
@@ -3,6 +3,7 @@ autoload -U colors && colors
 # TODO: colorize based on hostname
 # TODO: support for eg. `gcloud compute ssh ...`
 # TODO: alias as ssh
+# TODO: change LS_COLORS instead of terminal stuff, so it's cross-emulator compatible
 function colorssh() {
   sd '^colors: \*\w+$' 'colors: *red' ~/.config/alacritty/alacritty.yml
   trap "sd '^colors: \*\w+$' 'colors: *default' ~/.config/alacritty/alacritty.yml" EXIT
