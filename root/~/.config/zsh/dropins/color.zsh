4a06e1e758a8b4d5e4cf181b0421f836c3d7a55e
Kevin James <KevinJames@thekev.in>
2023-06-20 08:20:00 +0100

    feat(shell): configure LS_COLORS

--- root/~/.config/zsh/dropins/color.zsh
+++ root/~/.config/zsh/dropins/color.zsh
@@ -1,5 +1,11 @@
 autoload -U colors && colors
 
+# TODO: how best to handle PATH config order? Currently, dropin load order is
+# undefined (alphabetical?), an color.sh < xdg.sh, so vivid isn't yet on the
+# path.
+VIVID_DIR="${XDG_DATA_HOME}/cargo/bin"
+export LS_COLORS=$("${VIVID_DIR}/vivid" generate gruvbox-dark)
+
 # TODO: colorize based on hostname
 # TODO: support for eg. `gcloud compute ssh ...`
 # TODO: alias as ssh
