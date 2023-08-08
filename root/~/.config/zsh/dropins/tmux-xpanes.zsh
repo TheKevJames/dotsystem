1cc4d1864bc80e08ddca7368767b0e79c9c3b485
Kevin James <KevinJames@thekev.in>
2023-08-08 23:21:49 +0100

    feat(tmux): remove unused tmuxifier templates

--- root/~/.config/zsh/dropins/tmux-xpanes.zsh
+++ root/~/.config/zsh/dropins/tmux-xpanes.zsh
@@ -1,1 +1,13 @@
+# Open 16 panes:
+# $ xpanes {1..16}
+#
+# Connect to all redis nodes:
+# $ xpanes -c "gcloud compute ssh {}" redis-node-{0..11}
+#
+# Open all subdirectories:
+# $ fd -td -d1 . | xpanes -c "cd {}"
+# $ fd --hidden -td -d1 . ~/src/personal | xpanes -c "cd {}"
+# $ fd --no-ignore -td -d1 . ~/src/talkiq/algorithms | grep -vE '/(bin|mock|sample)/$' | xpanes -c "cd {}"
+# $ fd --no-ignore -td -d1 . ~/src/talkiq/gcloud-aio | grep -vE '/(bin|build|docs)/$' | xpanes -c "cd {}"
+
 export PATH="${XDG_SRC_HOME}/tmux-xpanes/bin:${PATH}"
