2ad2f77b3001f992285be52b3132beedc1620679
Kevin James <KevinJames@thekev.in>
2023-08-10 14:06:30 +0100

    feat(tmux): configure titles/colors for gcp, k8s, and ssh

--- root/~/.config/zsh/dropins/color.zsh
+++ root/~/.config/zsh/dropins/color.zsh
@@ -6,13 +6,64 @@ autoload -U colors && colors
 VIVID_DIR="${XDG_DATA_HOME}/cargo/bin"
 export LS_COLORS=$("${VIVID_DIR}/vivid" generate gruvbox-dark)
 
-# TODO: colorize based on hostname
-# TODO: support for eg. `gcloud compute ssh ...`
-# TODO: alias as ssh
-# TODO: change LS_COLORS instead of terminal stuff, so it's cross-emulator compatible
-function colorssh() {
-  sd '^colors: \*\w+$' 'colors: *red' ~/.config/alacritty/alacritty.yml
-  trap "sd '^colors: \*\w+$' 'colors: *default' ~/.config/alacritty/alacritty.yml" EXIT
+declare -A HOSTNAME_COLORS=(
+    ["applebox"]=green
+    ["pi-0"]=green
+    ["pi-1"]=green
+)
 
-  ssh $*
+function update_tmux_ssh() {
+    HOSTNAME="${1:-}"
+    tmux set-option -p @custom_pane_title "${HOSTNAME}"
+    tmux set-option -p @custom_pane_color "${HOSTNAME_COLORS[$HOSTNAME]:-terminal}"
 }
+
+function update_tmux_gcp() {
+    GCP=$(cat ~/.config/gcloud/active_config)
+    tmux set-option -g @custom_window_gcp_title "${GCP}"
+    if grep -E 'echelon|production' <(echo "${GCP}") >/dev/null; then
+        tmux set-option -g @custom_window_gcp_color 'red'
+    else
+        tmux set-option -g @custom_window_gcp_color 'green'
+    fi
+}
+
+function update_tmux_k8s() {
+    K8S=$(awk '/current-context:/ {print $2}' ~/.config/kube)
+    tmux set-option -g @custom_window_k8s_title "${K8S}"
+    if grep -E 'echelon|production' <(echo "${K8S}") >/dev/null; then
+        tmux set-option -g @custom_window_k8s_color 'red'
+    else
+        tmux set-option -g @custom_window_k8s_color 'green'
+    fi
+}
+
+function gcloud() {
+    if [[ "$@" =~ " ssh " ]]; then
+        echo update_tmux_ssh "${@:$#}"
+        echo running gcloud "$@"
+        echo update_tmux_ssh ""
+    else
+        echo gcloud "$@"
+    fi
+    update_tmux_gcp
+}
+
+function kubectl() {
+    command kubectl "$@"
+    update_tmux_k8s
+}
+
+function ssh() {
+    if [[ $(ps -p $(ps -p$$ -oppid=) -ocomm=) =~ "tmux" ]]; then
+        update_tmux_ssh "${@:$#} "
+        command ssh "$@"
+        update_tmux_ssh
+    else
+        command ssh "$@"
+    fi
+}
+
+# TODO: on tmux startup only
+update_tmux_gcp
+update_tmux_k8s
