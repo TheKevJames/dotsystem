bd6132e9aab479725ef822f9fa187895396c6910
Kevin James <KevinJames@thekev.in>
2024-01-28 19:49:13 +0000

    fix(xdg): fixup some xdg path refs

--- root/~/.config/zsh/completions/fzf.zsh
+++ root/~/.config/zsh/completions/fzf.zsh
@@ -1,2 +1,2 @@
 # shellcheck shell=bash source=/dev/null
-source "${XDG_DATA_HOME}/nvim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+source "${XDG_DATA_HOME}/nvim/lazy/fzf/shell/completion.zsh" 2> /dev/null
