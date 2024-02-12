74ca01d2da7eb6632258f52615ce4552cc5acde0
Kevin James <KevinJames@thekev.in>
2024-02-12 13:16:52 +0000

    chore(fzf): avoid installing unused files

--- root/~/.config/nvim/lua/plugins/fzf.lua
+++ root/~/.config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,7 @@
 return {
     {
         'junegunn/fzf',
-        build = './install --all',
+        build = './install --bin --no-fish',
         -- TODO: check out fzf-vim config options
         -- https://github.com/junegunn/fzf/blob/master/README-VIM.md
     },
