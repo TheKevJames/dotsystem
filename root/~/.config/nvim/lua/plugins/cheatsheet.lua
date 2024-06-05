c1323c1f9ff4dc6f8edd6f7a14591180c41e74f1
Kevin James <KevinJames@thekev.in>
2024-06-05 16:46:12 +0100

    feat(neovim): install cheatsheet.nvim

---
+++ root/~/.config/nvim/lua/plugins/cheatsheet.lua
@@ -0,0 +1,11 @@
+return {
+    'sudormrfbin/cheatsheet.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-lua/popup.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+    opts = {
+        bundled_cheatsheets = false,
+    },
+}
