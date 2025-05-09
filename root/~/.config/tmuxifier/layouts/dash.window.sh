e28a255bce9b0777645c40fe5f34c22033a1714b
Kevin James <KevinJames@thekev.in>
2025-05-09 17:14:26 +0100

    fix(tmux): fixup spaces and pane references

--- root/~/.config/tmuxifier/layouts/dash.window.sh
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -1,14 +1,14 @@
 new_window "dash"
 
 split_h 60
-select_pane 0
+select_pane 1
 split_v 30
-select_pane 2
+select_pane 3
 split_v 65
 
 # ...yeah.
-run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 0
-run_cmd "task due" 2
-run_cmd "cctui" 3
+run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 1
+run_cmd "task due" 3
+run_cmd "cctui" 4
 
-select_pane 2
+select_pane 3
