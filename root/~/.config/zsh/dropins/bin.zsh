b278d1906a3daf167c6306e65d5514810789caf2
Kevin James <KevinJames@thekev.in>
2025-08-27 17:56:54 +0100

    refactor(bin): fix SC2268

--- root/~/.config/zsh/dropins/bin.zsh
+++ root/~/.config/zsh/dropins/bin.zsh
@@ -34,7 +34,7 @@ fzfkill() {
     local pid
     pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
 
-    if [ "x$pid" != "x" ]; then
+    if [ "$pid" != "" ]; then
         echo "${pid}" | xargs kill -"${1:-9}"
     fi
 }
