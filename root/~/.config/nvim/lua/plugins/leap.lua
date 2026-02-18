6001ed2a5e4d220229b7106db71c3aa982360785
Kevin James <KevinJames@thekev.in>
2026-02-18 17:27:00 +0000

    feat(nvim): update leap config to non-deprecated bindings

--- root/~/.config/nvim/lua/plugins/leap.lua
+++ root/~/.config/nvim/lua/plugins/leap.lua
@@ -1,8 +1,12 @@
 return {
   {
-    'ggandor/leap.nvim',
+    'https://codeberg.org/andyg/leap.nvim',
     config = function()
-      require('leap').add_default_mappings({})
+      vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
+      vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
+      vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
+      vim.keymap.set({'x', 'o'}, 'x', '<Plug>(leap-forward-till)')
+      vim.keymap.set({'x', 'o'}, 'X', '<Plug>(leap-backward-till)')
     end,
   },
   {
