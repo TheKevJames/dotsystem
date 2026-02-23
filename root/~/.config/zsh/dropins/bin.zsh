5b3ea1b1a2680ca31eba1152d4d71b579e454cd3
Kevin James <KevinJames@thekev.in>
2026-02-23 20:28:18 +0000

    feat(fzf): bin ctrl-o to open in neovim

--- root/~/.config/zsh/dropins/bin.zsh
+++ root/~/.config/zsh/dropins/bin.zsh
@@ -10,31 +10,3 @@ pin() {
 pout() {
     cd "$(cat "${tmpdir}/pindir")" || return
 }
-
-# fzf a file and open it:
-# * CTRL-O to open with `open` command,
-# * CTRL-E or Enter key to open with the $EDITOR
-# Based on:
-# https://github.com/junegunn/fzf/wiki/Examples#opening-files
-fzfopen() {
-    read -rd '\n' key file <<<"$(fzf-tmux -- --query="${1:-.}" --exit-0 --expect=ctrl-e,ctrl-o)"
-    if [ -n "${key}" ]; then
-        if [ "${key}" = "ctrl-o" ]; then
-            open "${file}"
-        else
-            "${EDITOR:-vim}" "${file:-${key}}"
-        fi
-    fi
-}
-
-# fzf a process and kill it
-# Based on:
-# https://github.com/junegunn/fzf/wiki/Examples#processes
-fzfkill() {
-    local pid
-    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-
-    if [ "$pid" != "" ]; then
-        echo "${pid}" | xargs kill -"${1:-9}"
-    fi
-}
--- root/~/.config/zsh/dropins/fzf.zsh
+++ root/~/.config/zsh/dropins/fzf.zsh
@@ -1,4 +1,32 @@
 # shellcheck shell=bash
 export FZF_DEFAULT_COMMAND='fd -u --type f'
-export FZF_DEFAULT_OPTS='--height 40% --border'
+export FZF_DEFAULT_OPTS='--height 40% --border --bind="ctrl-o:become(nvim {+})"'
 export PATH="${XDG_DATA_HOME}/nvim/lazy/fzf/bin:${PATH}"
+
+# fzf a file and open it:
+# * CTRL-O to open with `open` command,
+# * CTRL-E or Enter key to open with the $EDITOR
+# Based on:
+# https://github.com/junegunn/fzf/wiki/Examples#opening-files
+fzfopen() {
+    read -rd '\n' key file <<<"$(fzf-tmux -- --query="${1:-.}" --exit-0 --expect=ctrl-e,ctrl-o)"
+    if [ -n "${key}" ]; then
+        if [ "${key}" = "ctrl-o" ]; then
+            open "${file}"
+        else
+            "${EDITOR:-vim}" "${file:-${key}}"
+        fi
+    fi
+}
+
+# fzf a process and kill it
+# Based on:
+# https://github.com/junegunn/fzf/wiki/Examples#processes
+fzfkill() {
+    local pid
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+    if [ "$pid" != "" ]; then
+        echo "${pid}" | xargs kill -"${1:-9}"
+    fi
+}
