db38404ba6516bc8857754a94325c7e12be4cf61
Kevin James <KevinJames@thekev.in>
2026-02-23 16:10:30 +0000

    feat(neovim): enable k8s crds in yaml lsp

--- root/~/.config/nvim/lsp/yamlls.lua
+++ root/~/.config/nvim/lsp/yamlls.lua
@@ -8,6 +8,9 @@ return {
     -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
     redhat = { telemetry = { enabled = false } },
     yaml = {
+      kubernetesCRDStore = {
+        enable = true,
+      },
       schemaStore = {
         enable = true,
         url = "https://www.schemastore.org/api/json/catalog.json",
