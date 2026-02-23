31e0671df5e7a9df61f3e3ddca0f3400d7f558a5
Kevin James <KevinJames@thekev.in>
2026-02-23 14:41:35 +0000

    feat: drop support for WSL <v17738

--- root/~/.config/zsh/dropins/linux.zsh
+++ root/~/.config/zsh/dropins/linux.zsh
@@ -1,11 +1,4 @@
 # shellcheck shell=bash
 if [[ "$OSTYPE" == "linux"* ]]; then
     alias open="xdg-open"
-
-    if [[ -f /proc/version ]]; then
-        if grep -q Microsoft /proc/version; then
-            # TODO: update to WSL >= v17738
-            unsetopt BG_NICE
-        fi
-    fi
 fi
