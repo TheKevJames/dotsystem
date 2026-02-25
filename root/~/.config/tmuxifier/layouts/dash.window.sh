1fce5e021c91c729809307eae38705dc6161d69f
Kevin James <KevinJames@thekev.in>
2026-02-25 20:02:37 +0000

    feat(mux): update dash window to include pi

--- root/~/.config/tmuxifier/layouts/dash.window.sh
+++ root/~/.config/tmuxifier/layouts/dash.window.sh
@@ -1,14 +1,12 @@
 new_window "dash"
 
 split_h 60
-select_pane 1
-split_v 30
-select_pane 3
+select_pane 2
 split_v 65
 
 # ...yeah.
 run_cmd $'nvim -c \'execute "normal \\\\ww"\'' 1
-run_cmd "task due" 3
-run_cmd "cctui" 4
+run_cmd "task due" 2
+run_cmd "pi --no-session" 3
 
-select_pane 3
+select_pane 2
