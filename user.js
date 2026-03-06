be14e9cd15be17046dedc59400ad66ff05ef4d50
Kevin James <KevinJames@thekev.in>
2026-03-06 09:55:03 +0000

    feat(firefox): backup firefox user prefs

---
+++ user.js
@@ -0,0 +1,27 @@
+// TODO: include this in dotsystem
+//
+// On OSX, this live in:
+//   ~/Library/Application Support/Firefox/Profiles/*.default-release/user.js
+
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("browser.ml.chat.menu", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("browser.search.visualSearch.featureGate", false);
+user_pref("browser.startup.homepage", "https://thekev.in/quotes");
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("com.text_fragments.enabled", false);
+user_pref("devtools.accessibility.enabled", false);
+user_pref("devtools.debugger.remote-enabled", false);
+user_pref("devtools.everOpened", true);
+user_pref("dom.text-recognition.enabled", false);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("privacy.query_stripping.strip_on_share.enabled", false);
+user_pref("screenshots.browser.component.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("userprof", true);
