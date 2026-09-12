8d4b9ddcd8b51854ac2f44774e1e1dfc015b3e9f
Kevin James <KevinJames@thekev.in>
2026-09-12 12:35:05 +0100

    feat(python): add -private aliases for using vi pypi

--- root/~/.config/zsh/dropins/pip.zsh
+++ root/~/.config/zsh/dropins/pip.zsh
@@ -1,2 +1,7 @@
 # shellcheck shell=bash
 export PIP_KEYRING_PROVIDER=subprocess
+
+export VOICEAI_PYPI="https://oauth2accesstoken@us-python.pkg.dev/voiceai-infra/voiceai-pypi/simple/"
+pip-private()  { PIP_EXTRA_INDEX_URL="$VOICEAI_PYPI" command pip "$@"; }
+pipx-private() { PIP_EXTRA_INDEX_URL="$VOICEAI_PYPI" command pipx "$@"; }
+uv-private() { PIP_EXTRA_INDEX_URL="$VOICEAI_PYPI" command uv "$@"; }
